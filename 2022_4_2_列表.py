b7e21314840ce502a9fd240c784974baea3d5a3e
清禾同学 <87123120+GeniusMa@users.noreply.github.com>
2022-04-02 17:22:36 +0800

    Add files via upload

---
+++ 2022_4_2_列表.py
@@ -0,0 +1,32 @@
+# This Python file uses the following encoding: utf-8
+
+# print [1,2,3,4,5]
+# list = [1,2,3,4,"AFAFA"]
+
+# print list
+
+# for i in list:
+# 	print i
+
+
+
+
+# list = [1,2,3,4,"AFAFA"]
+# for i in range(len(list)):
+# 	print list[i]
+
+# print list [-1]
+# print list[-2]
+
+
+
+list = [1,2,3,4,"AFAFA"]
+print list[0:3]
+print list[:3]
+print list[3:]
+print list[:]
+print list[0:5:2]
+print list[::-1]
+
+
+
