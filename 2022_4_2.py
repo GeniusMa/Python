f9acd1582b7f8631da85246170085504c69fe186
清禾同学 <87123120+GeniusMa@users.noreply.github.com>
2022-04-02 16:46:54 +0800

    Add files via upload

---
+++ 2022_4_2.py
@@ -0,0 +1,142 @@
+# This Python file uses the following encoding: utf-8
+
+
+# temp=input("输入数字")
+# guess=int(temp)
+# if guess == 8:
+# 	print("OK")
+# else:
+# 	print("8")
+# print("G")
+
+# x=3
+# print x 
+# y=5
+# x,y=y,x
+# print x,y
+# print "dadadadada"
+ 
+# print(r"D:\q\w\e\r\t")#r表示后面是一个原始字符串，转译字符不生效
+# print "  \n\
+#        *\n\
+#       ***\n\
+#      *****\n"
+
+# print """
+
+# a
+# b
+# c
+# d
+# e
+# f
+# """
+
+# print 520+1314
+
+# print "520"+"1314"
+
+# print "郭开颜是SB\n"*5
+
+
+# import random
+# a = random.randint(0,100)
+# print a
+
+# import decimal
+# print 0.1+0.2
+# a=decimal.Decimal('0.1')#实例化
+# b=decimal.Decimal('0.2')
+# print a+b
+# x=1+2j
+# print x.real
+# print x.imag
+
+# print 3/2
+# print 3//2#向下取整
+# print -3//2
+
+# print divmod(-3,2)#第一个返回值为//，第二个为%
+# #abs为BIF内置函数，不需要定义
+# print abs(1+2j)#复数的模
+
+# print int("520")
+# print int(1.99)
+# print int(1.1)
+# print float("3.14")
+# print float(520)
+# a=+1E6#科学计数法
+# print float(a)
+# print pow(2,3)
+# print 2**3
+# print pow(2,3,5)#==(2**2)%5
+
+
+
+
+# print bool(250)
+# print bool("Flase")
+# print bool(False)
+# print bool("")
+# print bool(" ")
+# print bool(0)
+# print bool(0.0)
+# print bool(0j)
+# print 1==True
+# print 0==False
+# print True+False
+
+
+# print 3<4 and 4<5
+# print 3<4 and 4>5
+
+# print 3<4 or 4<5
+# print 3<4 or 4>5
+
+# print not True
+# print not 1
+# print not 0
+
+# print 3 and 5
+# print 3 or 4
+# print 0 and 3
+# print 0 or 4#短路逻辑 输出影响结果的值
+
+
+
+
+
+# a=input()
+# b=input()
+# small=a if a<b else b
+# print small
+
+
+# score=input("请输入0-100的分数")
+# level=('D' if 0<=score<60 else
+# 	   'C' if 60<=score<80 else
+# 	   'B' if 80<=score<90 else
+# 	   'A' if 90<=score<100 else
+# 	   "请输入0-100的分数")
+# print level
+
+# for each in "olofmeister":
+# 	print each
+
+
+# for i in range(10):
+# 	print i
+
+# for i in range (5,10):
+# 	print i
+
+# for i in range(5 ,10,2):
+# 	print i
+
+sum =0;
+
+for i in range (100001):
+	sum+=i
+print sum
+
+
